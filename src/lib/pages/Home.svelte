3c0ce97d66f140b9dd04c9b6306c016350aee3cb
Angel <santoyox714@gmail.com>
2022-08-21 15:12:05 -0700

    Update Home.svelte

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -13,7 +13,7 @@
   id="bg"
 >
   <div class="text-center">
-    <h1 class="text-5xl m-6 {color} font-light">🖌️ Color-Craze 🎨  </h1>
+    <h1 class="text-5xl m-6 {color} font-light">🖌️ Color Craze 🎨  </h1>
   </div>
 
 </div>
